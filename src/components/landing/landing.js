fd9d58477a702ad9b080f81316ea09adc39910cc
Wayne Jones <waynejones@outlook.com>
2020-02-16 00:36:27 -0500

    Replace Bootstrap with Material UI

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BackgroundSlider from 'react-background-slider';
-// import { Carousel } from 'react-bootstrap';
-// import './landing.css';
+import { Box, Container } from '@material-ui/core/';
+import './landing.css';
 
 class Landing extends React.Component {
   constructor(props) {
@@ -9,9 +9,6 @@ class Landing extends React.Component {
     this.state = {
       title: 'Wayne Jones',
       description: 'Photographer, Videographer & Content Creator',
-      // carouselControls: false,
-      // carouselFade: true,
-      // carouselIndicators: false,
     };
   }
 
@@ -26,12 +23,21 @@ class Landing extends React.Component {
     const image4 = 'images/noiseporn-JNuKyKXLh8U-unsplash.jpg';
     const image5 = 'images/teemu-paananen-bzdhc5b3Bxs-unsplash.jpg';
     const image6 = 'images/tyler-franta-iusJ25iYu1c-unsplash.jpg';
+    const { title, description } = this.state;
     return (
-      <BackgroundSlider
-        images={[image1, image2, image3, image4, image5, image6]}
-        duration={8}
-        transition={1}
-      />
+      <>
+        <BackgroundSlider
+          images={[image1, image2, image3, image4, image5, image6]}
+          duration={8}
+          transition={1}
+        />
+        <Box>
+          <Container className="landing">
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </Container>
+        </Box>
+      </>
     );
   }
 }
