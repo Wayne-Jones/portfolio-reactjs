02c18ef512e649af03c4d2a276cb4f8b3e26a477
Wayne Jones <waynejones@nyu.edu>
2020-03-27 21:38:22 -0400

    Added Routes and responsive font theme from MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import Landing from './components/landing/landing';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  createMuiTheme, responsiveFontSizes, ThemeProvider,
+} from '@material-ui/core/styles';
+import Landing from './components/landing';
+import Photography from './components/photography';
+import Engineering from './components/engineering';
 import './App.css';
 
+
 function App() {
+  let theme = createMuiTheme();
+  theme = responsiveFontSizes(theme);
+
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/photography" exact component={Photography} />
+            <Route path="/engineering" exact component={Engineering} />
+          </Switch>
+        </ThemeProvider>
+      </div>
+    </Router>
+
   );
 }
 export default App;
